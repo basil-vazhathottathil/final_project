92b62273bec7d0b4d2eb97a813b4e3609422beba
Basil Vazhathottathil <basilveldose@gmail.com>
2026-01-14 07:35:39 +0530

    coooooooors

--- app/middleware/preflight.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Request
-from fastapi.responses import Response
-
-async def preflight_middleware(request: Request, call_next):
-    # Let CORS middleware handle headers, just unblock OPTIONS
-    if request.method == "OPTIONS":
-        return Response(status_code=204)
-
-    return await call_next(request)
